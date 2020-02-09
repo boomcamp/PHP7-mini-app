6a8b9827f5e38d3073cac8167a1ba80b784d6ca9
Jino Lacson <jinowilbertolacson@gmail.com>
2020-02-09 19:31:17 +0800

    Update ExampleClassInterface.php

--- classes/ExampleClassInterface.php
+++ classes/ExampleClassInterface.php
@@ -5,9 +5,9 @@ use Interfaces\IExample;
 
 class ExampleClassInterface implements IExample
 {
-	private $msg1;
+    private $msg1;
 
-	public function __construct($msg)
+    public function __construct($msg)
     {
         $this->msg1 = $msg;
     }
