fb99674c9cd0e453c0f685b9bd8b6409e1154457
Jino Lacson <jinowilbertolacson@gmail.com>
2020-03-13 17:48:18 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,10 @@
 #! /usr/bin/env bash
-
-# We need to install the project dependecies
 composer install
 composer dump-autoload --optimize
-
 ls .git
 ls .git/hooks
 mv .git/hooks/prepare-commit-msg.sample .git/hooks/prepare-commit-msg
 chmod +x .git/hooks/prepare-commit-msg
 cat pre-commit.sh >> .git/hooks/prepare-commit-msg
 ./pre-commit.sh
+git status -s
